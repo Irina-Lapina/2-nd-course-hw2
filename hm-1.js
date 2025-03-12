4dfdedb0384706f5f86eb50fe5a3fbd2e3a6cf2a
Irina-Lapina <tany076@mail.ru>
2025-03-13 01:10:36 +0700

    Проверьте дз

---
+++ hm-1.js
@@ -0,0 +1,48 @@
+let a = Number(alert('10'));
+a = 20;
+alert(a);
+
+let year = Number(alert('2007'));
+
+let nameJS = alert('Брэндан Эйх');
+
+let ten = 10;
+let two = 2;
+let theAmount = ten + two;
+alert(theAmount);
+let difference = ten - two;
+alert(difference);
+let multiplication = ten * two;
+alert(multiplication);
+let division = ten / two;
+alert(division);
+
+degree = 2 ** 5;
+let result = degree;
+alert(result);
+
+let a2 = 9; 
+let b = 2; 
+alert(a % b);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let age = Number(prompt('Сколько вам лет?'));
+alert(age);
+
+const user = {
+    name: 'Вася',
+    age: 20,
+    isAdminG: 'Да'
+}
+isAdminG = true;
+
+let name = prompt('Ваше имя?');
+alert('Привет, ' + name);
