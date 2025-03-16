d09c845b8a5511ed2d3d92e7681e477852b6219d
Irina-Lapina <tany076@mail.ru>
2025-03-16 22:01:24 +0700

    Четвёртая домашка готова

---
+++ hm-3.js
@@ -0,0 +1,39 @@
+for (let i = 0; i < 2; i++) {
+    console.log('Привет!');
+};
+
+let a = 1;
+do {
+    console.log(a);
+    a++;
+ } while(a <= 5);
+
+ let b = 7;
+do {
+    console.log(b);
+    b++;
+ } while(b <= 22);
+
+ let obj = {
+    "Коля": '200',
+    "Вася": '300',
+    "Петя": '400'
+ }
+ for (let key in obj) {
+    console.log(`${key} — зарплата ${obj[key]} долларов.`);
+ };
+
+ let n = 1000;
+ let num = 1;
+do {
+    n/= 2;
+    console.log(n);
+    console.log(num);
+    num++;
+} while (n >= 50);
+
+let dayFriday = 5;
+let allDay = 31;
+for (let f = dayFriday; f <= allDay ; f += 7) {
+    console.log(`Сегодня пятница, ${f}-е число. Необходимо подготовить отчет.`)
+}
