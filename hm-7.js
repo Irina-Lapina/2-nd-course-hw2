bc95f8ba28ba03caa0820efdea9fd7695bdef5c9
Irina-Lapina <tany076@mail.ru>
2025-04-17 01:27:15 +0700

    Последняя домашка в hm9

--- hm-7.js
+++ hm-7.js
@@ -49,93 +49,93 @@ function rockScissorsPaperGame() {
     } while (!regexp.test(playerEnter));
 }
 
-//Задание 1
+// //Задание 1
 
-const peopleList = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 }
- ];
+// const peopleList = [
+//     { name: 'Глеб', age: 29 },
+//     { name: 'Анна', age: 17 },
+//     { name: 'Олег', age: 7 },
+//     { name: 'Оксана', age: 47 }
+//  ];
  
- console.log(peopleList.sort((a, b) => a.age - b.age));
+//  console.log(peopleList.sort((a, b) => a.age - b.age));
 
- //Задание 2
+//  //Задание 2
 
-function isPositive(num) {
+// function isPositive(num) {
 
-return num > 0
-}
-function isMale(person) {
+// return num > 0
+// }
+// function isMale(person) {
 
-    return person.gender === 'male';
-}
+//     return person.gender === 'male';
+// }
 
-function filter(array, ruleFunction) {
-const result = [];
-for (let index = 0; index < array.length; index++) {
-    if (ruleFunction(array[index])) {
-        result.push(array[index]);
-    }
+// function filter(array, ruleFunction) {
+// const result = [];
+// for (let index = 0; index < array.length; index++) {
+//     if (ruleFunction(array[index])) {
+//         result.push(array[index]);
+//     }
     
-}
-return result;
-}
+// }
+// return result;
+// }
 
-console.log(filter([3, -4, 1, 9], isPositive));
+// console.log(filter([3, -4, 1, 9], isPositive));
 
-const people = [
-   {name: 'Глеб', gender: 'male'},
-   {name: 'Анна', gender: 'female'},
-   {name: 'Олег', gender: 'male'},
-   {name: 'Оксана', gender: 'female'}
-];
+// const people = [
+//    {name: 'Глеб', gender: 'male'},
+//    {name: 'Анна', gender: 'female'},
+//    {name: 'Олег', gender: 'male'},
+//    {name: 'Оксана', gender: 'female'}
+// ];
 
-console.log(filter(people, isMale));
+// console.log(filter(people, isMale));
 
-//Задание 3
+// //Задание 3
 
-const timerAlarm = () => {
-    const interval = setInterval(() => {
-        console.log(new Date());
-    }, 3000);
+// const timerAlarm = () => {
+//     const interval = setInterval(() => {
+//         console.log(new Date());
+//     }, 3000);
 
-    setTimeout(() => {
-        clearInterval(interval);
-        console.log("30 секунд прошло");
-    }, 30000);
-
-
-}
-timerAlarm(30);
+//     setTimeout(() => {
+//         clearInterval(interval);
+//         console.log("30 секунд прошло");
+//     }, 30000);
 
 
-//Задание 4
+// }
+// timerAlarm(30);
 
-function delayForSecond(callback) {
+
+// //Задание 4
+
+// function delayForSecond(callback) {
    
-   setTimeout(() => {
-    callback();
+//    setTimeout(() => {
+//     callback();
     
-   }, 1000);
-}
+//    }, 1000);
+// }
 
-delayForSecond(function () {
-   console.log('Привет, Глеб!');
-})
+// delayForSecond(function () {
+//    console.log('Привет, Глеб!');
+// })
 
-//Задание 5
+// //Задание 5
 
-function delayForSecond(cb) {
-    setTimeout(() => {
-        console.log('Прошла одна секунда');
-        if(cb) {  cb(); }
-    }, 1000)
-}
+// function delayForSecond(cb) {
+//     setTimeout(() => {
+//         console.log('Прошла одна секунда');
+//         if(cb) {  cb(); }
+//     }, 1000)
+// }
 
-function sayHi (name) {
-    console.log(`Привет, ${name}!`);
-}
+// function sayHi (name) {
+//     console.log(`Привет, ${name}!`);
+// }
 
 
-delayForSecond(() => sayHi('Глеб'));
+// delayForSecond(() => sayHi('Глеб'));
